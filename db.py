f3305a592db637197416fcde367e170aad19fefc
ksw22 <hanksw22@gmail.com>
2025-09-04 18:52:32 +0900

    영철쓰 커밋

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ conn = psycopg2.connect(
     host="localhost",          # 또는 실제 DB 서버 주소
     port="5432",               # 기본 포트
     database="riskqueens",     # 사용할 데이터베이스 이름
-    user="postgres",      # 사용자 이름
+    user="ubuntu",      # 사용자 이름
     password="0000"   # 비밀번호
 )
 
